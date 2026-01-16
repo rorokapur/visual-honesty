03c0a5a8fe6b9f7133fff44e2f748b1fe602f45e
Rohan Kapur <kapurkrohan@gmail.com>
2026-01-16 01:40:12 -0800

    Add basic survey UI with database integration (#1)

---
+++ src/components/SurveyContainer.tsx
@@ -0,0 +1,109 @@
+import { Center, Loader, Title } from "@mantine/core";
+import { useState } from "react";
+import { STIMULI_SET } from "../data/stimuli";
+import { supabase } from "../lib/supabase";
+import { Trial } from "./Trial";
+
+interface SurveyContainerProps {
+  /**
+   * Unique session identifier for the participant.
+   * Prevents duplicate submissions and helps group responses together in the db.
+   */
+  session: string;
+
+  /**
+   * Whether the participant has already taken the survey or not.
+   * Prevents duplicate submissions to the database to save resources.
+   */
+  hasTaken?: boolean;
+}
+
+/**
+ * Main Visual Honesty survey component.
+ * Handles trial progression, user selections, and data submission to Supabase.
+ * @component
+ */
+export function SurveyContainer({ session, hasTaken }: SurveyContainerProps) {
+  // Current index in the set of images/trials
+  const [stimulusIndex, setStimulusIndex] = useState<number>(0);
+  // Whether the right/left order of images is flipped
+  const [isFlipped, setIsFlipped] = useState(() => Math.random() < 0.5);
+  // Loading state for async operations
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const isFinished = stimulusIndex >= STIMULI_SET.length;
+
+  /**
+   * Processes user selection for the current trial and sends results to Supabase.
+   * @param {'left' | 'right'} choice - the graph the user selected (as being deceptive)
+   */
+  const handleSelect = async (choice: "left" | "right") => {
+    setLoading(true);
+
+    // Determine whether selected image was honest or deceptive
+    const selection =
+      choice === "left"
+        ? isFlipped
+          ? "deceptive"
+          : "honest"
+        : isFlipped
+        ? "honest"
+        : "deceptive";
+
+    // Skip sending results if survey already taken
+    // TODO: add some kind of flash or animation between trials when skipping for consistency
+    if (!hasTaken) {
+      const { error } = await supabase.from("responses").insert([
+        {
+          session_id: session, // Unique UUID for this participant
+          pair_id: STIMULI_SET[stimulusIndex].id, // e.g., 'pair_01'
+          selected_answer: selection, // 'honest' or 'deceptive'
+          selected_side: choice, // Did they physically click left or right?
+        },
+      ]);
+
+      if (error) {
+        console.error("Submission failed:", error.message);
+      }
+    }
+
+    // Advance to next trial
+    setStimulusIndex(stimulusIndex + 1);
+    setIsFlipped(Math.random() < 0.5);
+    setLoading(false);
+  };
+
+  // Show loader while submitting (prevents multiple API calls)
+  if (loading) {
+    return (
+      <Loader
+        size="xl"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      />
+    );
+  }
+
+  // Show completion message if finished
+  if (isFinished) {
+    // Set flag in storage to prevent submissions on retakes
+    localStorage.setItem("vh_taken", "true");
+    return (
+      <Center>
+        <Title>Survey Complete! Thank you for your participation.</Title>
+      </Center>
+    );
+  }
+
+  return (
+    <Trial
+      stimulus={STIMULI_SET[stimulusIndex]}
+      onSelect={handleSelect}
+      isFlipped={isFlipped}
+    ></Trial>
+  );
+}
---
+++ src/components/Trial.tsx
@@ -0,0 +1,65 @@
+import { Card, Center, Image, SimpleGrid, Space, Text } from "@mantine/core";
+import type { Stimulus } from "../data/stimuli";
+
+interface TrialProps {
+  /**
+   * The pair of images to display for this trial.
+   */
+  stimulus: Stimulus;
+
+  /**
+   * Callback to handle user answer selection.
+   * @param choice - the stimulus side selected by the user
+   */
+  onSelect: (choice: "left" | "right") => void;
+
+  /**
+   * Whether or not to flip the left/right order of the images from the default.
+   */
+  isFlipped: boolean;
+}
+
+/**
+ * A component containing an individual trial of the Visual Honesty survey
+ * @component
+ */
+export function Trial({ stimulus, onSelect, isFlipped }: TrialProps) {
+  return (
+    <>
+      <Center>
+        <Text size="xl">Select the deceptive visualization below</Text>
+      </Center>
+      <Space h="xl" />
+      <SimpleGrid cols={2} spacing="xl">
+        <Card
+          shadow="sm"
+          padding="lg"
+          withBorder
+          radius="lg"
+          onClick={() => onSelect("left")}
+        >
+          <Image
+            src={isFlipped ? stimulus.deceptiveImage : stimulus.honestImage}
+            alt="Stimulus A"
+            draggable={false}
+            style={{ userSelect: "none" }}
+          />
+        </Card>
+        <Card
+          shadow="sm"
+          padding="lg"
+          withBorder
+          radius="lg"
+          onClick={() => onSelect("right")}
+        >
+          <Image
+            src={isFlipped ? stimulus.honestImage : stimulus.deceptiveImage}
+            alt="Stimulus B"
+            draggable={false}
+            style={{ userSelect: "none" }}
+          />
+        </Card>
+      </SimpleGrid>
+    </>
+  );
+}
