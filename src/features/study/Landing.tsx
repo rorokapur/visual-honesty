29b1db2c5f86c2ab1e09e0dd090149c49c09f24c
Rohan Kapur <kapurkrohan@gmail.com>
2026-02-05 11:17:48 -0800

    update landing text

--- src/features/study/Landing.tsx
+++ src/features/study/Landing.tsx
@@ -1,4 +1,13 @@
-import { Button, Center, Container, Stack, Text, Title } from "@mantine/core";
+import {
+  Button,
+  Card,
+  Center,
+  Container,
+  Space,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 
 interface LandingProps {
   /** Callback to start the survey */
@@ -12,27 +21,37 @@ export function Landing({ handleStart }: LandingProps) {
       <header style={{ background: "white" }}>
         <Container px="md">
           <Center style={{ padding: "16px 0" }}>
-            <Title ta="center">Can you spot the deceptive visualization?</Title>
+            <Title ta="center">Are you fooled by deceptive charts?</Title>
           </Center>
         </Container>
       </header>
       <main>
-        <Container size="sm" px="md">
-          <Stack align="center" gap="md">
-            <Text>
-              This is some text that will eventually be replaced with a proper
-              description of the experiement being conducted and its background.
-              There may even be an example of a deceptive visualization seen in
-              the wild.
-            </Text>
-            <Text>
-              The following test will show you pairs of data visualizations.
-              Your goal is to identify which graph is trying to decieve you or
-              misrepresent the data. Click on the graph that you think is the
-              most deceptive. You will receive a score at the end of the test.
-            </Text>
-            <Button onClick={handleStart}>Start</Button>
-          </Stack>
+        <Container size="sm" px="md" bg="">
+          <Card withBorder radius="lg">
+            <Stack align="center" gap="md">
+              <Text>
+                Graphs and charts are supposed to simplify the truth, but they
+                are often used to hide it. We’ve created a series of charts to
+                test your ability to spot 'dishonest' representations of data.
+                It only takes 5 minutes, and your results will help us better
+                understand the visual deception. See how you score against your
+                friends, family, and other users who take the test!
+              </Text>
+              <Text>
+                The following test will show you pairs of charts displaying the
+                same data. Your goal is to identify which one is trying to
+                decieve you or misrepresent its data. For each pair displayed,
+                click on the graph that you think is the most deceptive. At the
+                end of the test, your results will be displayed.
+              </Text>
+            </Stack>
+          </Card>
+          <Space h="lg"></Space>
+          <Center>
+            <Button size="md" onClick={handleStart}>
+              Start Test
+            </Button>
+          </Center>
         </Container>
       </main>
     </>
